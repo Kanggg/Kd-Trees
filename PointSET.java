526a386bd9357c34c9fe2ae752b3baa3c7300f1e
kangli <likangxd@gmail.com>
2016-12-29 09:57:37 -0800

    Add methods: draw(), nearest(); \nHandle null pointer

--- PointSET.java
+++ PointSET.java
@@ -5,7 +5,6 @@ import edu.princeton.cs.algs4.Queue;
 import java.util.Iterator;
 import java.util.TreeSet;
 
-
 public class PointSET {
 	private TreeSet<Point2D> set;
 
@@ -26,21 +25,35 @@ public class PointSET {
 
 	// add the point to the set (if it is not already in the set)
 	public void insert(Point2D p) {
+		if (p == null) throw new java.lang.NullPointerException();
+		
 		set.add(p);
 	}
 
 	// does the set contain point p? 
 	public boolean contains(Point2D p) {
+		if (p == null) throw new java.lang.NullPointerException();
+		
 		return set.contains(p);
 	}
 
 	// draw all points to standard draw 
 	public void draw() {
-
+		Queue<Point2D> all = new Queue<Point2D>();
+		Iterator<Point2D> iterate = set.iterator();
+		
+		while (iterate.hasNext()) {
+			all.enqueue(iterate.next());
+		}
+		for (Point2D p : all) {
+			p.draw();
+		}
 	}
 
 	// all points that are inside the rectangle 
 	public Iterable<Point2D> range(RectHV rect) {
+		if (rect == null) throw new java.lang.NullPointerException();
+		
 		Queue<Point2D> all = new Queue<Point2D>();
 		Queue<Point2D> ret = new Queue<Point2D>();
 
@@ -58,7 +71,26 @@ public class PointSET {
 
 	// a nearest neighbor in the set to point p; null if the set is empty 
 	public Point2D nearest(Point2D p) {
+		if (p == null) throw new java.lang.NullPointerException();
+		
+		if (set.isEmpty()) return null;
 
+		Queue<Point2D> all = new Queue<Point2D>();
+		Iterator<Point2D> iterate = set.iterator();
+		while (iterate.hasNext()) {
+			all.enqueue(iterate.next());
+		}
+		
+		double min = Double.MAX_VALUE;
+		Point2D ret = null;
+		for (Point2D point : all) {
+			double distance = point.distanceTo(p);
+			if (min > distance) {
+				min = distance;
+				ret = point;
+			}
+		}
+		
+		return ret;
 	}
-
 }
